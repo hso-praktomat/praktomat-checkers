ab4eec41f7aa5d4162bcda2b2e22546dfd90399d
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2023-01-17 17:40:59 +0100

    exercise.yml: support for default values from outer dicts

--- multi-checker/script/exercise.py
+++ multi-checker/script/exercise.py
@@ -5,6 +5,25 @@ from utils import *
 from shell import *
 import yaml
 import re
+from typing import *
+
+class YamlDict:
+    def __init__(self, dicts: Union[dict, list[dict]]):
+        if isinstance(dicts, list):
+            self.__dicts = dicts
+        else:
+            self.__dicts = [dicts]
+    def get(self, key, default=None):
+        for d in self.__dicts:
+            if key in d:
+                return d[key]
+        return default
+    def items(self):
+        return self.__dicts[0].items()
+    def extend(self, d):
+        if d is None:
+            raise ValueError('d must not be None')
+        return YamlDict([d] + self.__dicts)
 
 @dataclass
 class Assignment:
@@ -15,23 +34,16 @@ class Assignment:
     tests: list[str] # test files, can be empty
     testOkRequired: bool
     testScript: Optional[str]
-    def parse(sheet: str, v: dict, id: int, defSrc: str):
-        if v:
-            src = v.get('src', defSrc)
-            tests = asList(v.get('test', [])) + asList(v.get('tests', []))
-            points = v.get('points', -1)
-            try:
-                points = int(points)
-            except ValueError:
-                bug('error parsing exercise.yaml: points must be a number')
-            testOkRequired = v.get('test-ok-required', False)
-            testScript = v.get('test-script')
-        else:
-            src = defSrc
-            tests = []
-            points = 0
-            testOkRequired = False
-            testScript = None
+    def parse(sheet: str, v: YamlDict, id: int):
+        src = v.get('src')
+        tests = asList(v.get('test', [])) + asList(v.get('tests', []))
+        points = v.get('points', -1)
+        try:
+            points = int(points)
+        except ValueError:
+            bug('error parsing exercise.yaml: points must be a number')
+        testOkRequired = v.get('test-ok-required', False)
+        testScript = v.get('test-script')
         return Assignment(sheet, id, points, src, tests, testOkRequired, testScript)
 
 assignmentIdRe = re.compile(r'\d+[a-z]?')
@@ -40,19 +52,18 @@ assignmentIdRe = re.compile(r'\d+[a-z]?')
 class Exercise:
     sheet: str
     assignments: list[Assignment]
-    def parse(sheet: str, ymlDict: dict) -> Exercise:
-        defSrc = ymlDict.get('src')
+    def parse(sheet: str, ymlDict: YamlDict) -> Exercise:
         assignments = []
         for k, v in ymlDict.items():
             if type(k) == int:
                 k = str(k)
             if type(k) != str or not assignmentIdRe.match(k):
                 continue # not an assignment
-            a = Assignment.parse(sheet, v, k, defSrc)
+            a = Assignment.parse(sheet, ymlDict.extend(v), k)
             assignments.append(a)
         return Exercise(sheet, assignments)
 
 def parseExercise(sheet, yamlPath):
     s = readFile(yamlPath)
     ymlDict = yaml.load(s, Loader=yaml.FullLoader)
-    return Exercise.parse(sheet, ymlDict)
+    return Exercise.parse(sheet, YamlDict(ymlDict))
