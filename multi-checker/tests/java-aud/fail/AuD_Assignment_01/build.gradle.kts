a4c40a7c9b145edb4b4b0a8945eb575c880348cc
Hannes Braun <hannes@hannesbraun.net>
2024-09-11 15:11:39 +0200

    Update to Java 21

--- multi-checker/tests/java-aud/fail/AuD_Assignment_01/build.gradle.kts
+++ multi-checker/tests/java-aud/fail/AuD_Assignment_01/build.gradle.kts
@@ -35,8 +35,8 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.test {
