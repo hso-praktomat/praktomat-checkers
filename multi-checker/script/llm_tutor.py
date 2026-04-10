e17a1b8516a39606c0ac4734e4be30ca006a24b2
Tasnim-Alaref <talaref@stud.hs-offenburg.de>
2026-04-10 21:24:48 +0200

    update

--- multi-checker/script/llm_tutor.py
+++ multi-checker/script/llm_tutor.py
@@ -75,11 +75,3 @@ def check(opts: LlmTutorOptions):
                             studentSolution= student_pfad,
                             pdf_task= pdf)
 
-"""
-docker run --volume C:\Users\Lenovo\Documents\Uni_Offenburg\7.semester\Bachelor_Wehr\praktomat\praktomat-checkers\multi-checker\tests\llm-tutor:/external:ro \
-    —-volume C:\Users\Lenovo\Documents\Uni_Offenburg\7.semester\Bachelor_Wehr\praktomat\praktomat-checkers\multi-checker\tests\llm-tutor\loesung:/solution:ro \ 
- 	—-volume C:\Users\Lenovo\Documents\Uni_Offenburg\7.semester\Bachelor_Wehr\praktomat\praktomat-checkers\multi-checker\tests\llm-tutor\pdfs:/pdf:ro \ 
-    --workdir /path/to/student/submission \
-    python3 /checker/check.py llm-tutor 
-
-"""
